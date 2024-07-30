8016dfee309bf7584176755c0ee77a16bf972470
Yanghaolong <yanghaolong2@huawei.com>
2024-07-30 12:54:56 +0000

    高清低码key
    Signed-off-by: Yanghaolong <yanghaolong2@huawei.com>

--- interface/inner_api/meta/video_types.h
+++ interface/inner_api/meta/video_types.h
@@ -93,6 +93,7 @@ enum VideoEncodeBitrateMode : int32_t {
     CBR = 0, // constant bit rate mode.
     VBR = 1, // variable bit rate mode.
     CQ = 2, // constant quality mode.
+    CBR_VideoCall = 3, // constant bit rate mode with Rlambda.
 };
 
 enum class ColorPrimary : int32_t {
