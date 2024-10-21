d79372d853ccf40992ae1d137c07a48dd0ac5b2e
xuzhangchi <xuzhangchi@huawei.com>
2024-10-21 14:27:08 +0800

    VIDEO_DECODER_ENABLE_VRR -> VIDEO_DECODER_OUTPUT_ENABLE_VRR
    Signed-off-by: xuzhangchi <xuzhangchi@huawei.com>

--- interface/inner_api/meta/meta_key.h
+++ interface/inner_api/meta/meta_key.h
@@ -469,8 +469,8 @@ public:
     static constexpr const char AV_TRANSCODER_VIDEO_ENCODER_DURATION[] = "av_transcoder_video_encoder_duration";
     static constexpr const char AV_TRANSCODER_VIDEO_VPE_DURATION[] = "av_transcoder_video_vpe_duration";
 /* -------------------- vrr tag -------------------- */
-    static constexpr const char VIDEO_DECODER_ENABLE_VRR[] = 
-        "video_decoder_enable_vrr"; ///< int32_t, 1 is enabled, 0 is unabled
+    static constexpr const char VIDEO_DECODER_OUTPUT_ENABLE_VRR[] = 
+        "video_decoder_output_enable_vrr"; ///< int32_t, 1 is enabled, 0 is unabled
 };
 
 using TagTypeCharSeq = const char *;
