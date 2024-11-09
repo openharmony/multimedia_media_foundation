b9561cca4f915386b8d442b8ddc0c37d2f9b91d2
SuRuoyan <suruoyan1@huawei.com>
2024-11-10 00:44:12 +0800

    add vendor aac encoder
    Signed-off-by: SuRuoyan <suruoyan1@huawei.com>

--- src/plugin/plugin_list.cpp
+++ src/plugin/plugin_list.cpp
@@ -36,7 +36,7 @@ std::vector<PluginDescription> PluginList::GetAllPlugins()
     return pluginDescriptionList_;
 }
 
-PluginDescription PluginList::GetPluginByCap(PluginType pluginType, std::string needCap)
+std::vector<PluginDescription> PluginList::GetPluginsByCap(PluginType pluginType, std::string needCap)
 {
     std::vector<PluginDescription> matchedPlugins;
     std::vector<PluginDescription>::iterator it;
@@ -47,15 +47,11 @@ PluginDescription PluginList::GetPluginByCap(PluginType pluginType, std::string 
             matchedPlugins.push_back(temp);
         }
     }
-    int32_t maxRank = 0;
-    PluginDescription bestMatchedPlugin;
-    for (it = matchedPlugins.begin(); it != matchedPlugins.end(); it++) {
-        PluginDescription temp = *it;
-        if (temp.rank > maxRank) {
-            bestMatchedPlugin = temp;
-        }
-    }
-    return bestMatchedPlugin;
+    std::sort(matchedPlugins.begin(), matchedPlugins.end(),
+        [](PluginDescription a, PluginDescription b) {
+            return a.rank > b.rank;
+        });
+    return matchedPlugins;
 }
 
 PluginDescription PluginList::GetPluginByName(std::string name)
@@ -93,6 +89,7 @@ PluginList::PluginList()
     AddFFmpegDemuxerPlugins();
     AddFFmpegAudioDecodersPlugins();
     AddAudioVividDecodersPlugins();
+    AddAudioVendorAacEncodersPlugin();
     AddG711muAudioDecoderPlugins();
     AddLbvcAudioDecoderPlugins();
     AddOpusAudioDecoderPlugins();
@@ -499,6 +496,17 @@ void PluginList::AddFFmpegAudioEncodersPlugins()
     flacAudioEncoderPlugin.cap = "audio/flac";
     flacAudioEncoderPlugin.rank = DEFAULT_RANK;
     pluginDescriptionList_.push_back(flacAudioEncoderPlugin);
+}
+
+void PluginList::AddAudioVendorAacEncodersPlugin()
+{
+    PluginDescription AacEncoderPlugin;
+    AacEncoderPlugin.pluginName = "OH.Media.Codec.Encoder.Audio.Vendor.AAC";
+    AacEncoderPlugin.packageName = "AudioVendorAacEncoder";
+    AacEncoderPlugin.pluginType = PluginType::AUDIO_ENCODER;
+    AacEncoderPlugin.cap = "audio/mp4a-latm";
+    AacEncoderPlugin.rank = DEFAULT_RANK + 1; // larger than default aac plugin
+    pluginDescriptionList_.push_back(AacEncoderPlugin);
 }
 
 void PluginList::AddG711muAudioEncoderPlugins()
