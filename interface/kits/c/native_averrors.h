55cea325da38df5001ac90f216eacb18ac326cd7
chongzhi <caihongzhi@huawei.com>
2024-11-12 15:46:32 +0800

    Player错误码细分
    Signed-off-by: chongzhi <caihongzhi@huawei.com>

--- interface/kits/c/native_averrors.h
+++ interface/kits/c/native_averrors.h
@@ -93,6 +93,173 @@ typedef enum OH_AVErrCode {
      * @since 12
      */
     AV_ERR_VIDEO_UNSUPPORTED_COLOR_SPACE_CONVERSION = 301,
+    /**
+     * @the address of server is incorrect, and IO can not find host.
+     * @since 14
+     */
+    AV_ERR_IO_CANNOT_FIND_HOST = 5411001,
+    /**
+     * @network connection timeout.
+     * @since 14
+     */
+    AV_ERR_IO_CONNECTION_TIMEOUT = 5411002,
+    /**
+     * @failed link due to abnormal network.
+     * @since 14
+     */
+    AV_ERR_IO_NETWORK_ABNORMAL = 5411003,
+    /**
+     * @failed link due to unavailable network.
+     * @since 14
+     */
+    AV_ERR_IO_NETWORK_UNAVAILABLE = 5411004,
+    /**
+     * @network permission dennied.
+     * @since 14
+     */
+    AV_ERR_IO_NO_PERMISSION = 5411005,
+    /**
+     * @the client request parameters are incorrect or exceed the processing capacity.
+     * @since 14
+     */
+    AV_ERR_IO_NETWORK_ACCESS_DENIED = 5411006,
+    /**
+     * @cannot find available network resources.
+     * @since 14
+     */
+    AV_ERR_IO_RESOURE_NOT_FOUND = 5411007,
+    /**
+     * @the server failS to verify the client certificate because the certificate is not carried,
+     *  the certificate is invalid, or the certificate is expired.
+     * @since 14
+     */
+    AV_ERR_IO_SSL_CLIENT_CERT_NEEDED = 5411008,
+    /**
+     * @the client fails to verify the server certificate because the certificate is not carried,
+     *  the certificate is invalid, or the certificate is expired.
+     * @since 14
+     */
+    AV_ERR_IO_SSL_CONNECT_FAIL = 5411009,
+    /**
+     * @IO SSL server cert untrusted.
+     * @since 14
+     */
+    AV_ERR_IO_SSL_SERVER_CERT_UNTRUSTED = 5411010,
+    /**
+     * @unsupported request due to network protocols.
+     * @since 14
+     */
+    AV_ERR_IO_UNSUPPORTTED_REQUEST = 5411011,
+    /**
+     * @file data is incorrect, and no specific data can be provided.
+     * @since 14
+     */
+    AV_ERR_IO_DATA_ABNORMAL = 5411012,
+    /**
+     * @file is occupied by other processes and cannot be accessed.
+     * @since 14
+     */
+    AV_ERR_IO_FILE_ACCESS_DENIED = 5411013,
+    /**
+     * @the file handle is incorrect.
+     * @since 14
+     */
+    AV_ERR_IO_FILE_BAD_HANDLE = 5411014,
+    /**
+     * @IO file not found.
+     * @since 14
+     */
+    AV_ERR_IO_FILE_NOT_FOUND = 5411015,
+    /**
+     * @IO file permission denied because API does not have permissions.
+     * @since 14
+     */
+    AV_ERR_IO_FILE_PERMISSION_DENIED = 5411016,
+    /**
+     * @fail to decode the audio data.
+     * @since 14
+     */
+    AV_ERR_IO_AUDIO_DEC_FAILED = 5411017,
+    /**
+     * @fail to init the audio decoder.
+     * @since 14
+     */
+    AV_ERR_IO_AUDIO_DEC_INIT_FAILED = 5411018,
+    /**
+     * @the audio decoder is unavailable for handler incurrupt, or audio decoder subsystem abnormal.
+     * @since 14
+     */
+    AV_ERR_IO_AUDIO_DEC_UNAVAILABLE = 5411019,
+    /**
+     * @audio device error.
+     * @since 14
+     */
+    AV_ERR_IO_AUDIO_DEVICE_ERROR = 5411020,
+    /**
+     * @audio device state does not support the current operation.
+     * @since 14
+     */
+    AV_ERR_IO_AUDIO_DEVICE_INVALID_STATE = 5411021,
+    /**
+     * @audio device timeout when calling audio render interface.
+     * @since 14
+     */
+    AV_ERR_IO_AUDIO_DEVICE_TIMEOUT = 5411022,
+    /**
+     * @audio device unavailable, unable to create an audio device.
+     * @since 14
+     */
+    AV_ERR_IO_AUDIO_DEVICE_UNAVAILABLE = 5411023,
+    /**
+     * @fail to encode the audio data.
+     * @since 14
+     */
+    AV_ERR_IO_AUDIO_ENC_FAILED = 5411024,
+    /**
+     * @fail to init the audio encoder.
+     * @since 14
+     */
+    AV_ERR_IO_AUDIO_ENC_INIT_FAILED = 5411025,
+    /**
+     * @audio encoder is unavailable for handler incurrupt, or audio encoder subsystem abnormal.
+     * @since 14
+     */
+    AV_ERR_IO_AUDIO_ENC_UNAVAILABLE = 5411026,
+    /**
+     * @fail to decode the video data.
+     * @since 14
+     */
+    AV_ERR_IO_VIDEO_DEC_FAILED = 5411027,
+    /**
+     * @fail to init the video decoder.
+     * @since 14
+     */
+    AV_ERR_IO_VIDEO_DEC_INIT_FAILED = 5411028,
+    /**
+     * @video decoder is unavailable for handler incurrupt, or video decoder subsystem abnormal.
+     * @since 14
+     */
+    AV_ERR_IO_VIDEO_DEC_UNAVAILABLE = 5411029,
+    /**
+     * @video device error.
+     * @since 14
+     */
+    AV_ERR_IO_VIDEO_DEVICE_ERROR = 5411030,
+    /**
+     * @fail to encode the video data.
+     * @since 14
+     */
+    AV_ERR_IO_VIDEO_ENC_FAILED = 5411031,
+    /**
+     * @fail to init the video encoder.
+     * @since 14
+     */
+    AV_ERR_IO_VIDEO_ENC_INIT_FAILED = 5411032,
+    /**
+     * @video encoder is unavailable for handler incurrupt, or video encoder subsystem abnormal.
+     * @since 14
+     */
+    AV_ERR_IO_VIDEO_ENC_UNAVAILABLE = 5411033,
 } OH_AVErrCode;
 
 #ifdef __cplusplus
