719e75174de4ca6c2fc5824c301291c46c2dbf4c
彼此彼此 <chenyuchen8@huawei.com>
2024-12-12 09:39:54 +0800

    add audio vivid demuxer
    Signed-off-by: 彼此彼此 <chenyuchen8@huawei.com>

--- interface/inner_api/meta/meta_key.h
+++ interface/inner_api/meta/meta_key.h
@@ -150,7 +150,10 @@ public:
         "audio_object_number_key"; ///< Key for audio object number
     static constexpr const char OH_MD_KEY_AUDIO_VIVID_METADATA[] =
         "audio_vivid_metadata_key"; ///< Key for audio vivid metadata
-
+    static constexpr const char AUDIO_SOUNDBED_CHANNELS_NUMBER[] =
+        "audio.soundbed.channels.number"; ///< Key for audio soundbed channels number
+    static constexpr const char AUDIO_HOA_ORDER[] =
+        "audio.hoa.order"; ///< Key for audio hoa order
     /* -------------------- video universal tag -------------------- */
     static constexpr const char VIDEO_WIDTH[] = "width";                               ///< video width
     static constexpr const char VIDEO_HEIGHT[] = "height";                             ///< video height
