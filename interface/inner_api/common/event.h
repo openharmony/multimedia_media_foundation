6f24b86517d2462ae451947926abffa11b17d8a6
hanwenzhao <hanwenzhao@huawei.com>
2024-12-30 15:49:16 +0800

    video play dfx -- record perf data
    Signed-off-by: hanwenzhao <hanwenzhao@huawei.com>
    Change-Id: I9ff34caa780bca46fa530a9342478ced3f23416d

--- interface/inner_api/common/event.h
+++ interface/inner_api/common/event.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023-2023 Huawei Device Co., Ltd.
+ * Copyright (c) 2023-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -60,6 +60,7 @@ enum struct DfxEventType : uint32_t {
     DFX_INFO_PLAYER_AUDIO_LAG,
     DFX_INFO_PLAYER_STREAM_LAG,
     DFX_INFO_PLAYER_EOS_SEEK,
+    DFX_INFO_PERF_REPORT,
     DFX_EVENT_BUTT,
 };
 
