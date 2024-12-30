72c7f474cb98919b5015e1d3befa53394d86aaa8
hanwenzhao <hanwenzhao@huawei.com>
2024-12-30 15:49:16 +0800

    video play dfx -- record perf data
    Signed-off-by: hanwenzhao <hanwenzhao@huawei.com>
    Change-Id: I9ff34caa780bca46fa530a9342478ced3f23416d

--- interface/inner_api/filter/filter.h
+++ interface/inner_api/filter/filter.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023-2023 Huawei Device Co., Ltd.
+ * Copyright (c) 2023-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -162,6 +162,10 @@ public:
 
     virtual Status WaitAllState(FilterState state) final;
 
+    virtual Status SetPerfRecEnabled(bool isPerfRecEnabled) final;
+
+    virtual Status DoSetPerfRecEnabled(bool isPerfRecEnabled);
+
     virtual Status DoInitAfterLink();
 
     virtual Status DoPrepare();
@@ -244,6 +248,8 @@ protected:
     std::map<StreamType, std::vector<std::shared_ptr<FilterLinkCallback>>> linkCallbackMaps_;
 
     std::string groupId_;
+
+    bool isPerfRecEnabled_ { false };
 
 private:
     void ChangeState(FilterState state);
--- interface/inner_api/plugin/plugin_event.h
+++ interface/inner_api/plugin/plugin_event.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023-2023 Huawei Device Co., Ltd.
+ * Copyright (c) 2023-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -60,6 +60,22 @@ struct PluginEvent {
     PluginEventType type;
     Any param;
     std::string description;
+};
+
+enum PluginDfxEventType : int32_t {
+    SOURCE,
+    DEMUXER,
+    VIDEO_DECODER,
+    AUDIO_DECODER,
+    VIDEO_SINK,
+    AUDIO_SINK,
+    VIDEO_RENDERER,
+    AUDIO_RENDERER,
+};
+
+struct PluginDfxEvent {
+    PluginDfxEventType type;
+    Any param;
 };
 } // namespace Plugins
 } // namespace Media
