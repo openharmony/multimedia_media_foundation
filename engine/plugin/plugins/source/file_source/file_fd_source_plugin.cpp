b0bd87a49ba7312c67229afa0c7acd140f5cc7fb
Feng Lin <linfeng67@huawei.com>
2025-01-24 08:32:42 +0000

    descrip
    Signed-off-by: Feng Lin <linfeng67@huawei.com>

--- engine/plugin/plugins/source/file_source/file_fd_source_plugin.cpp
+++ engine/plugin/plugins/source/file_source/file_fd_source_plugin.cpp
@@ -164,7 +164,9 @@ Status FileFdSourcePlugin::ParseUriInfo(const std::string& uri)
         Status::ERROR_INVALID_PARAMETER, "Invalid fd: " PUBLIC_LOG_D32, fd_);
     fileSize_ = GetFileSize(fd_);
     if (fdUriMatch.size() == 4) { // 4：4 sub match
-        offset_ = std::stoll(fdUriMatch[2].str()); // 2: sub match offset subscript
+        std::string offsetStr = fdUriMatch[2].str(); // 2: sub match offset subscript
+        FALSE_RETURN_V_MSG_E(StrToLong(offsetStr, offset_), Status::ERROR_INVALID_PARAMETER,
+            "Failed to read offset.");
         if (static_cast<uint64_t>(offset_) > fileSize_) {
             offset_ = fileSize_;
         }
