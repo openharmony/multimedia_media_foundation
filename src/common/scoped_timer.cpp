8888a2603152ec841c0c7706566643ab0c75c786
x00911906 <xulei272@huawei.com>
2025-03-29 19:16:03 +0800

    dfx_mf329
    Signed-off-by: x00911906 <xulei272@huawei.com>

---
+++ src/common/scoped_timer.cpp
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "scoped_timer.h"
+#include "log.h"
+#include "plugin/plugin_time.h"
+
+namespace {
+constexpr OHOS::HiviewDFX::HiLogLabel LABEL = {LOG_CORE, LOG_DOMAIN_PLAYER, "ScopedTimer"};
+}
+
+namespace OHOS {
+namespace Media {
+ScopedTimer::ScopedTimer(const std::string& name, int64_t timeout)
+    : name_(name), timeout_(timeout), start_(Plugins::GetCurrentMillisecond()) {}
+
+ScopedTimer::~ScopedTimer()
+{
+    auto endTime = Plugins::GetCurrentMillisecond();
+    auto duration = endTime - start_;
+    FALSE_RETURN(duration >= 0);
+    FALSE_RETURN(duration > timeout_);
+    MEDIA_LOG_W("name: " PUBLIC_LOG_S ", time-taking: " PUBLIC_LOG_D64 " ms", name_.c_str(), duration);
+}
+
+}
+}
