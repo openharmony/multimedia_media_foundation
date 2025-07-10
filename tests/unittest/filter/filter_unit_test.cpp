c903b5591bf6b8b33b5bb37aef4b92c573760329
zss <5583155+zss1209@user.noreply.gitee.com>
2025-07-10 14:58:23 +0800

    视频mv播放开关能力
    Signed-off-by: zss <zhangshuangshuang14@h-partners.com>

--- tests/unittest/filter/filter_unit_test.cpp
+++ tests/unittest/filter/filter_unit_test.cpp
@@ -355,6 +355,89 @@ HWTEST_F(FilterUnitTest, WaitAllState_004, TestSize.Level1)
     EXPECT_EQ(FilterState::INITIALIZED, filter->curState_);
     EXPECT_EQ(Status::OK, filter->WaitAllState(FilterState::INITIALIZED));
 }
+
+/**
+ * @tc.name: ReleaseOnMuted_001
+ * @tc.desc: Test ReleaseOnMuted interface
+ * @tc.type: FUNC
+ */
+HWTEST_F(FilterUnitTest, ReleaseOnMuted_001, TestSize.Level1)
+{
+    std::shared_ptr<Filter> filter = std::make_shared<Filter>("testFilter", FilterType::FILTERTYPE_VENC, true);
+    std::shared_ptr<Filter> filter2 = std::make_shared<Filter>("testFilter", FilterType::FILTERTYPE_VENC, true);
+    filter->Init(nullptr, nullptr);
+    filter2->Init(nullptr, nullptr);
+    filter->nextFiltersMap_[StreamType::STREAMTYPE_PACKED].push_back(filter2);
+    filter->LinkPipeLine("");
+    EXPECT_EQ(Status::OK, filter->Start());
+    EXPECT_EQ(Status::OK, filter->Flush());
+    EXPECT_EQ(Status::OK, filter->ReleaseOnMuted());
+}
+
+/**
+ * @tc.name: ReleaseOnMuted_002
+ * @tc.desc: Test ReleaseOnMuted interface
+ * @tc.type: FUNC
+ */
+HWTEST_F(FilterUnitTest, ReleaseOnMuted_002, TestSize.Level1)
+{
+    std::shared_ptr<Filter> filter = std::make_shared<Filter>("testFilter", FilterType::FILTERTYPE_VENC, true);
+    std::shared_ptr<Filter> filter2 = std::make_shared<Filter>("testFilter", FilterType::FILTERTYPE_VENC, true);
+    filter->Init(nullptr, nullptr);
+    filter2->Init(nullptr, nullptr);
+    filter->nextFiltersMap_[StreamType::STREAMTYPE_PACKED].push_back(filter2);
+    filter->LinkPipeLine("");
+    sleep(sleepTime_);
+    EXPECT_EQ(Status::OK, filter->Start());
+    sleep(sleepTime_);
+    EXPECT_EQ(Status::OK, filter->Flush());
+    sleep(sleepTime_);
+    EXPECT_EQ(Status::OK, filter->ReleaseOnMuted());
+    sleep(sleepTime_);
+}
+
+/**
+ * @tc.name: ReInitAndStart_001
+ * @tc.desc: Test ReInitAndStart interface
+ * @tc.type: FUNC
+ */
+HWTEST_F(FilterUnitTest, ReInitAndStart_001, TestSize.Level1)
+{
+    std::shared_ptr<Filter> filter = std::make_shared<Filter>("testFilter", FilterType::FILTERTYPE_VENC, true);
+    std::shared_ptr<Filter> filter2 = std::make_shared<Filter>("testFilter", FilterType::FILTERTYPE_VENC, true);
+    filter->Init(nullptr, nullptr);
+    filter2->Init(nullptr, nullptr);
+    filter->nextFiltersMap_[StreamType::STREAMTYPE_PACKED].push_back(filter2);
+    filter->LinkPipeLine("");
+    EXPECT_EQ(Status::OK, filter->Start());
+    EXPECT_EQ(Status::OK, filter->Flush());
+    EXPECT_EQ(Status::OK, filter->ReleaseOnMuted());
+    EXPECT_EQ(Status::OK, filter->ReInitAndStart());
+}
+
+/**
+ * @tc.name: ReInitAndStart_002
+ * @tc.desc: Test ReInitAndStart interface
+ * @tc.type: FUNC
+ */
+HWTEST_F(FilterUnitTest, ReInitAndStart_002, TestSize.Level1)
+{
+    std::shared_ptr<Filter> filter = std::make_shared<Filter>("testFilter", FilterType::FILTERTYPE_VENC, true);
+    std::shared_ptr<Filter> filter2 = std::make_shared<Filter>("testFilter", FilterType::FILTERTYPE_VENC, true);
+    filter->Init(nullptr, nullptr);
+    filter2->Init(nullptr, nullptr);
+    filter->nextFiltersMap_[StreamType::STREAMTYPE_PACKED].push_back(filter2);
+    filter->LinkPipeLine("");
+    sleep(sleepTime_);
+    EXPECT_EQ(Status::OK, filter->Start());
+    sleep(sleepTime_);
+    EXPECT_EQ(Status::OK, filter->Flush());
+    sleep(sleepTime_);
+    EXPECT_EQ(Status::OK, filter->ReleaseOnMuted());
+    sleep(sleepTime_);
+    EXPECT_EQ(Status::OK, filter->ReInitAndStart());
+    sleep(sleepTime_);
+}
 } // namespace FilterUnitTest
 } // namespace Media
 } // namespace OHOS
