f7a1068beaf5b65f5a33ef55b391ec133a64ffa7
lzm <linziming2@huawei.com>
2025-08-19 14:43:11 +0800

    新增图形像素格式key
    Change-Id: Ib09fed4ab0e50d7d49ba99c56ebbabf3e3d676f5
    Signed-off-by: lzm <linziming2@huawei.com>

--- src/meta/meta.cpp
+++ src/meta/meta.cpp
@@ -93,6 +93,7 @@ DEFINE_METADATA_SETTER_GETTER_FUNC(VideoEncodeBitrateMode, int32_t)
 DEFINE_METADATA_SETTER_GETTER_FUNC(TemporalGopReferenceMode, int32_t)
 DEFINE_METADATA_SETTER_GETTER_FUNC(VideoEncodeBFrameGopMode, int32_t)
 DEFINE_METADATA_SETTER_GETTER_FUNC(AudioChannelLayout, int64_t)
+DEFINE_METADATA_SETTER_GETTER_FUNC(GraphicPixelFormat, int32_t)
 
 #define  DEFINE_METADATA_SETTER_GETTER(tag, EnumType) {tag, std::make_pair(Set##EnumType, Get##EnumType)}
 
@@ -118,6 +119,7 @@ static std::map<TagType, std::pair<MetaSetterFunction, MetaGetterFunction>> g_me
     DEFINE_METADATA_SETTER_GETTER(Tag::VIDEO_ENCODE_BITRATE_MODE, VideoEncodeBitrateMode),
     DEFINE_METADATA_SETTER_GETTER(Tag::VIDEO_ENCODE_B_FRAME_GOP_MODE, VideoEncodeBFrameGopMode),
     DEFINE_METADATA_SETTER_GETTER(Tag::VIDEO_ENCODER_TEMPORAL_GOP_REFERENCE_MODE, TemporalGopReferenceMode),
+    DEFINE_METADATA_SETTER_GETTER(Tag::VIDEO_GRAPHIC_PIXEL_FORMAT, GraphicPixelFormat),
 };
 
 using  MetaSetterInt64Function = std::function<bool(Meta&, const TagType&, int64_t&)>;
@@ -248,6 +250,7 @@ static Any defaultTemporalGopReferenceMode = TemporalGopReferenceMode::ADJACENT_
 static Any defaultAudioChannelLayout = AudioChannelLayout::UNKNOWN;
 static Any defaultAudioAacProfile = AudioAacProfile::ELD;
 static Any defaultAudioAacStreamFormat = AudioAacStreamFormat::ADIF;
+static Any defaultGraphicPixelFormat = GraphicPixelFormat::GRAPHIC_PIXEL_FMT_YCBCR_420_SP;
 static Any defaultVectorUInt8 = std::vector<uint8_t>();
 static Any defaultVectorUInt32 = std::vector<uint32_t>();
 static Any defaultVectorInt32 = std::vector<int32_t>();
@@ -271,6 +274,7 @@ static std::map<TagType, const Any &> g_metadataDefaultValueMap = {
     {Tag::VIDEO_ENCODE_BITRATE_MODE, defaultVideoEncodeBitrateMode},
     {Tag::VIDEO_ENCODE_B_FRAME_GOP_MODE, defaultVideoEncodeBFrameGopMode},
     {Tag::VIDEO_ENCODER_TEMPORAL_GOP_REFERENCE_MODE, defaultTemporalGopReferenceMode},
+    {Tag::VIDEO_GRAPHIC_PIXEL_FORMAT, defaultGraphicPixelFormat},
     // Int8
     {Tag::RECORDER_HDR_TYPE, defaultInt8},
     // Uint_8
