9024194cbc662e837bb68dee93814d73032ddfae
qq_63432433 <pengyixuan3@h-partners.com>
2025-11-26 09:43:39 +0800

    update: 更新文件 media_monitor_info.h
    Signed-off-by: qq_63432433 <pengyixuan3@h-partners.com>

--- services/media_monitor/common/include/media_monitor_info.h
+++ services/media_monitor/common/include/media_monitor_info.h
@@ -104,10 +104,6 @@ enum VolumeStatisticsSceneType : uint8_t {
     LOUD_VOLUME_SCENE = 0,
 };
 
-VolumeStatisticsSceneType sceneTypes[] = {
-    VolumeStatisticsSceneType::LOUD_VOLUME_SCENE
-};
-
 enum EventType {
     UNKNOW_EVENTTYPE = -1,
     FAULT_EVENT = 0,
