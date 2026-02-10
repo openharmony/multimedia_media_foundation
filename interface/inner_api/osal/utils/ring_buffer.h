d9b0f6f7ff9fc738797813ec4f93c5460d726a5d
yu_qi0 <suyixin9@h-partners.com>
2026-02-10 15:40:48 +0800

    6.1蓝黄同步
    Signed-off-by: yu_qi0 <suyixin9@h-partners.com>

--- interface/inner_api/osal/utils/ring_buffer.h
+++ interface/inner_api/osal/utils/ring_buffer.h
@@ -84,7 +84,7 @@ public:
         }
         MEDIA_LOG_D("WriteBuffer in current tail " PUBLIC_LOG_ZU ", head_ " PUBLIC_LOG_ZU, tail_, head_);
         if (writeSize > SIZE_MAX - tail_) {
-            MEDIA_LOG_W("WriteBuffer WriteSize overflow " PUBLIC_LOG_ZU ", tail " PUBLIC_LOG_ZU, writeSize, tail_);
+            MEDIA_LOG_W("WriteBuffer writeSize overflow " PUBLIC_LOG_ZU ", tail " PUBLIC_LOG_ZU, writeSize, tail_);
             return false;
         }
         while (writeSize + tail_ > head_ + bufferSize_) {
