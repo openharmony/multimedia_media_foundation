ad033410f1a85de90a71e896ab4c9c46df3767eb
gcw_4z3jiPvB <huweinan1@h-partners.com>
2026-04-27 22:16:28 +0800

    update: 更新文件 aac_demuxer_plugin.cpp
    Signed-off-by: gcw_4z3jiPvB <huweinan1@h-partners.com>

--- engine/plugin/plugins/demuxer/aac_demuxer/aac_demuxer_plugin.cpp
+++ engine/plugin/plugins/demuxer/aac_demuxer/aac_demuxer_plugin.cpp
@@ -338,10 +338,8 @@ int AACDemuxerPlugin::AudioDemuxerAACPrepare(const uint8_t *buf, uint32_t len, A
         }
 
         int sampleIndex = ((buf[2] & 0x3C) >> 2); // 根据协议计算 sampleIndex
-        if (sampleIndex >= static_cast<int>(sizeof(samplingRateMap) / sizeof(samplingRateMap[0]))) {
-            MEDIA_LOG_E("Invalid sampleIndex " PUBLIC_LOG_D32, sampleIndex);
-            return -1;
-        }
+        FALSE_RETURN_V_MSG_E(sampleIndex < static_cast<int>(sizeof(samplingRateMap) / sizeof(samplingRateMap[0])),
+            -1, "Invalid sampleIndex.");
         int channelCount = ((buf[2] & 0x01) << 2) | ((buf[3] & 0xC0) >> 6); // 根据协议计算 channelCount
 
         int sample = samplingRateMap[sampleIndex];
