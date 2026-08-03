b26ff29b90b5fe5a4b744ad4e7969f9168af047c
wangyongli66 <wangyongli7@h-partners.com>
2026-08-03 11:23:07 +0800

    update: 更新文件 media_core.h
    Signed-off-by: wangyongli66 <wangyongli7@h-partners.com>

--- interface/inner_api/common/media_core.h
+++ interface/inner_api/common/media_core.h
@@ -140,6 +140,8 @@ enum MediaServiceErrCode : ErrCode {
     MSERR_FRAMEWORK_ERROR,                              // Framework error.
     MSERR_VID_CAPTURE_CONFIG_FAILED,                    // Video capture configure failed.
     MSERR_INVALID_TIMESTAMP,                            // Data error, timestamp is invalid.
+    MSERR_VIDEO_RESOLUTION_OUT_OF_RANGE,                // Video resolution is out of range.
+    MSERR_TARGET_RESOLUTION_OUT_OF_RANGE,               // Target resolution is out of range.
     MSERR_INVALID_FD,                                   // Invalid fd.
     MSERR_INVALID_WRITE,                                // File descriptor is not in read-write mode or write-only mode.
     MSERR_INVALID_AUD_BITRATE,                          // Invalid audioBitrate.
